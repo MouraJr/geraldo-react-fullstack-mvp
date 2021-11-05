37398b1d9d7161370b9c0ff5cdbff1337803a9eb
Geraldo Moura <geraldo.moura@ymail.com>
2021-11-05 09:30:43 -0500

    creating components, some style

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api'
+
+const Chart = () => {
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData())
+        }
+
+        fetchAPI()
+    });
+
+    return (
+        <div>
+            <h1>Charts</h1>
+        </div>
+    )
+}
+
+export default Chart
---
+++ src/components/CityPicker/CityPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CityPicker = () => {
+    return (
+        <div>
+            <h1>City Picker</h1>
+        </div>
+    )
+}
+
+export default CityPicker
